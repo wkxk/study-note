3bda3a19984e253d431950026bd377f391263108
wukai08 <wukai08@ky-tech.com.cn>
2021-04-07 17:41:46 +0800

    commit

---
+++ part3/fed-e-task-03-01/vue-study/minivue/js/observer.js
@@ -0,0 +1,30 @@
+class Observer {
+  constructor (data) {
+    this.walk(data)
+  }
+  walk (data) {
+    // 1.判断data是否为对象
+    if (!data || typeof data !== 'object') return
+    // 2.遍历data所有属性
+    Object.keys(data).forEach(key => {
+      this.defineRactive(data, key, data[key])
+    })
+  }
+
+  defineRactive (obj, key, val) {
+    // 如果val是对象，把val内部的属性转化为响应式数据
+    this.walk(val)
+    Object.defineProperty(obj, key, {
+      enumerable: true,
+      configurable: true,
+      get () {
+        return val
+      },
+      set (newValue) {
+        if (newValue === val) return
+        val = newValue
+        // 发送通知
+      }
+    })
+  }
+}
